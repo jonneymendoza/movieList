904d19d0cb460af1815b0655c000eb1bf4574bdc
Jonathan Richards <jrichards1408@gmail.com>
2019-01-07 00:18:46 +0000

    no message

--- app/src/main/java/com/richards/jonathan/movielist/data/entity/MovieList.kt
+++ app/src/main/java/com/richards/jonathan/movielist/data/entity/MovieList.kt
@@ -1,2 +1,6 @@
 package com.richards.jonathan.movielist.data.entity
 
+data class MovieList(val page: Long,
+                     val totalResults: Long,
+                     val totalPages: Long,
+                     val results: MutableList<MovieItem>)
